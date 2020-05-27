fca6f1ed556af7779bcfc7f3fa67f83d23eae2fd
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2020-05-27 22:54:45 +0530

    mobile site added

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const openPage = (x)=>{
     document.getElementById(`${x}-container`).scrollIntoView();
-}
+    closeMenu();
+};
+
+const openMenu = () => {
+    document.getElementById('nav-menu').style.display = 'flex';
+};
+const closeMenu = () => {
+    document.getElementById('nav-menu').style.display = 'none';
+};
